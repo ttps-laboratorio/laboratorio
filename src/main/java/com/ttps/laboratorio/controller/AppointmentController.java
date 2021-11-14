bebc0e0309553add4ca00def1d788ec2c4b96747
Sofia Scotto <sofia.scotto@mercadolibre.com>
2021-11-14 02:39:27 -0300

    Added appointment controller and service

---
+++ src/main/java/com/ttps/laboratorio/controller/AppointmentController.java
@@ -0,0 +1,63 @@
+package com.ttps.laboratorio.controller;
+
+import com.ttps.laboratorio.service.AppointmentService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.lang.NonNull;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "appointment")
+public class AppointmentController {
+
+  private final AppointmentService appointmentService;
+
+  public AppointmentController (AppointmentService appointmentService) {
+    this.appointmentService = appointmentService;
+  }
+
+  /**
+   * View a list of all appointments.
+   * @return  Returns a list of all appointments with "200 OK".
+   */
+  @PreAuthorize("hasRole('CONFIGURATOR') OR hasRole('EMPLOYEE')")
+  @GetMapping(path = "/")
+  public ResponseEntity<?> listAppointments() {
+    return ResponseEntity.ok(appointmentService.getAllAppointments());
+  }
+
+  /**
+   * Returns a boolean list, where each index represents a day of the month.
+   * If index 5 is false then an appointment can not be scheduled on that day.
+   * @return  Returns a list of all appointments with "200 OK".
+   */
+  @PreAuthorize("hasRole('CONFIGURATOR') OR hasRole('EMPLOYEE')")
+  @GetMapping(path = "/")
+  public ResponseEntity<?> listFreeAppointmentDaysByMonth(@RequestParam(name = "year") @NonNull Integer year, @RequestParam(name = "month") @NonNull Integer month) {
+    return ResponseEntity.ok(appointmentService.getFreeAppointmentDaysByMonth(year, month));
+  }
+
+  /**
+   * Returns a list of the appointments of a day.
+   * @return  Returns a list of all appointments with "200 OK".
+   */
+  @PreAuthorize("hasRole('CONFIGURATOR') OR hasRole('EMPLOYEE')")
+  @GetMapping(path = "/")
+  public ResponseEntity<?> listAppointmentsByDate(@RequestParam(name = "year") @NonNull Integer year, @RequestParam(name = "month") @NonNull Integer month, @RequestParam(name = "day") @NonNull Integer day) {
+    return ResponseEntity.ok(appointmentService.getAppointmentsByDate(year, month, day));
+  }
+
+  /**
+   * Returns a list of the available appointments of a day.
+   * @return  Returns a list of all appointments with "200 OK".
+   */
+  @PreAuthorize("hasRole('CONFIGURATOR') OR hasRole('EMPLOYEE')")
+  @GetMapping(path = "/")
+  public ResponseEntity<?> listAvailableAppointmentsByDate(@RequestParam(name = "year") @NonNull Integer year, @RequestParam(name = "month") @NonNull Integer month, @RequestParam(name = "day") @NonNull Integer day) {
+    return ResponseEntity.ok(appointmentService.getAvailableAppointmentsByDate(year, month, day));
+  }
+
+}
---
+++ src/main/java/com/ttps/laboratorio/service/AppointmentService.java
@@ -0,0 +1,109 @@
+package com.ttps.laboratorio.service;
+
+import com.ttps.laboratorio.entity.Appointment;
+import com.ttps.laboratorio.entity.BlockedDay;
+import com.ttps.laboratorio.entity.ScheduleConfigurator;
+import com.ttps.laboratorio.repository.IAppointmentRepository;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.YearMonth;
+import java.time.ZoneId;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AppointmentService {
+
+  private final IAppointmentRepository appointmentRepository;
+
+  private final BlockedDayService blockedDayService;
+
+  public AppointmentService(IAppointmentRepository appointmentRepository,
+                            BlockedDayService blockedDayService) {
+    this.appointmentRepository = appointmentRepository;
+    this.blockedDayService = blockedDayService;
+  }
+
+  /**
+   * Gets all appointments registered.
+   *
+   * @return List of all appointments
+   */
+  public List<Appointment> getAllAppointments() {
+    return new ArrayList<>(appointmentRepository.findAll());
+  }
+
+  public List<Boolean> getFreeAppointmentDaysByMonth(Integer year, Integer month) {
+    int daysInMonth = YearMonth.of(year, month).lengthOfMonth();
+    List<Boolean> freeDaysInMonth = new ArrayList<>(Arrays.asList(new Boolean[daysInMonth]));
+    Collections.fill(freeDaysInMonth, Boolean.TRUE);
+    Calendar calendar = Calendar.getInstance();
+    // filters weekends
+    blockSaturdaysAndSundays(calendar, year, month, daysInMonth, freeDaysInMonth);
+    List<BlockedDay> blockedDaysFromMonth = blockedDayService.getBlockedDaysByMonth(month);
+    // filters blocked days
+    blockedDaysFromMonth.forEach(blockedDay -> blockBlockedDays(blockedDay, freeDaysInMonth));
+    // filters appointment full days
+    blockAppointmentFullDays(calendar, year, month, daysInMonth, freeDaysInMonth);
+    return freeDaysInMonth;
+  }
+
+  public List<Appointment> getAppointmentsByDate(Integer year, Integer month, Integer day) {
+    LocalDate date = LocalDate.of(year, month, day);
+    List<Appointment> allAppointments = getAllAppointments();
+    return allAppointments.stream().filter(appointment -> appointment.getDate().equals(date)).collect(Collectors.toList());
+  }
+
+  public List<LocalTime> getAvailableAppointmentsByDate(Integer year, Integer month, Integer day) {
+    List<Appointment> appointmentsFromDate = getAppointmentsByDate(year, month, day);
+    List<LocalTime> appointmentList = getRawAppointmentList();
+    if (appointmentsFromDate != null && !appointmentsFromDate.isEmpty()) {
+      appointmentsFromDate.forEach(appointment -> appointmentList.removeIf(a -> a.equals(appointment.getTime())));
+    }
+    return appointmentList;
+  }
+
+  private void blockSaturdaysAndSundays(Calendar calendar, Integer year, Integer month, int daysInMonth, List<Boolean> freeDaysInMonth) {
+    for (int day = 1; day <= daysInMonth; day++) {
+      calendar.set(year, month, day);
+      int dayOfWeek = calendar.get(Calendar.DAY_OF_WEEK);
+      if (dayOfWeek == Calendar.SATURDAY || dayOfWeek == Calendar.SUNDAY) {
+        freeDaysInMonth.set(day, Boolean.FALSE);
+      }
+    }
+  }
+
+  private void blockBlockedDays(BlockedDay blockedDay, List<Boolean> freeDaysInMonth) {
+    LocalDate localDate = blockedDay.getDate().toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+    int day = localDate.getDayOfMonth();
+    freeDaysInMonth.set(day, Boolean.FALSE);
+  }
+
+  private void blockAppointmentFullDays(Calendar calendar, Integer year, Integer month, int daysInMonth, List<Boolean> freeDaysInMonth) {
+    for (int day = 1; day <= daysInMonth; day++) {
+      calendar.set(year, month, day);
+      List<Appointment> appointmentsFromDate = getAppointmentsByDate(year, month, day);
+      if (appointmentsFromDate != null && appointmentsFromDate.isEmpty()) {
+        freeDaysInMonth.set(day, Boolean.FALSE);
+      }
+    }
+  }
+
+  //TODO its not checking if the scheduleConfigurator may have a change
+  private List<LocalTime> getRawAppointmentList() {
+    ScheduleConfigurator scheduleConfigurator = new ScheduleConfigurator();
+    LocalTime aux = scheduleConfigurator.getOpeningTime();
+    List<LocalTime> rawAppointmentList = new ArrayList<>();
+    while (aux.compareTo(scheduleConfigurator.getClosingTime()) < 0) {
+      rawAppointmentList.add(aux);
+      aux = aux.plusMinutes(scheduleConfigurator.getFrequency().getMinute());
+    }
+    return rawAppointmentList;
+  }
+
+}
