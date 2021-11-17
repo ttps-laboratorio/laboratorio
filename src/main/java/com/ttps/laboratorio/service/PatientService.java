12c4dc0a66a098f3ba0bc80d5816a217a8c23c66
Sofia Scotto <sofia.scotto@mercadolibre.com>
2021-11-17 10:21:57 -0300

    Contact dto

--- src/main/java/com/ttps/laboratorio/service/PatientService.java
+++ src/main/java/com/ttps/laboratorio/service/PatientService.java
@@ -52,9 +52,9 @@ public class PatientService {
 
   private void setPatient(Patient patient, PatientDTO request) {
     Contact contact = new Contact();
-    contact.setName(request.getContactName());
-    contact.setEmail(request.getContactEmail());
-    contact.setPhoneNumber(request.getContactPhoneNumber());
+    contact.setName(request.getContact().getName());
+    contact.setEmail(request.getContact().getEmail());
+    contact.setPhoneNumber(request.getContact().getPhoneNumber());
     patient.setDni(request.getDni());
     patient.setFirstName(request.getFirstName());
     patient.setLastName(request.getLastName());
